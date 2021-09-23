cb81181b35481b8f613c9777151b123b8cc4b0fa
Bryan Serna Osorio <bryanseros@gmail.com>
2021-09-23 07:46:38 -0500

    Iniciar proyecto con inyección de dependencias

---
+++ src/main/java/com/bolsadeideas/springboot/di/app/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.bolsadeideas.springboot.di.app.controller;
+
+import com.bolsadeideas.springboot.di.app.models.service.MiServicio;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    private MiServicio servicio = new MiServicio();
+
+    @GetMapping({"/","","/index"})
+    public String index(Model model){
+
+        return "index";
+    }
+}
